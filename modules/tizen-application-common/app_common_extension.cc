c1de74cb8c7f2845bfa45303b8e8c723ce74defc
Seungkeun Lee <sngn.lee@samsung.com>
2016-01-05 11:25:55 +0900

    New AppFW Module implements
     - initial version
     - xwalk style moudle
    
    Change-Id: I0b40628790f2acf79699595d324b53b9164463d4

---
+++ modules/tizen-application-common/app_common_extension.cc
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "app_common_extension.h"
+
+#include <dlog.h>
+#include <app_control.h>
+#include <app_common.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#define LOG_TAG "JSNative"
+
+namespace appfw {
+
+xwalk::XWalkExtensionInstance* AppCommonExtension::CreateInstance() {
+  return new AppCommonInstance();
+}
+
+void AppCommonInstance::Initialize() {
+  LOGD("Created tizen-application-common instance");
+}
+
+void AppCommonInstance::HandleMessage(const char* msg) {
+  // parse json object
+}
+
+void AppCommonInstance::HandleSyncMessage(const char* msg) {
+  // parse json object
+  picojson::value value;
+  std::string err;
+  picojson::parse(value, msg, msg + strlen(msg), &err);
+  if (!err.empty()) {
+    LOGE("Ignoring message. Can't parse msessage : %s", err.c_str());
+    return;
+  }
+  if (!value.is<picojson::object>()) {
+    LOGE("Ignoring message. It is not an object.");
+    return;
+  }
+
+  // handle synchronous messages
+  std::string cmd = value.get("cmd").to_str();
+  picojson::object result;
+  if (cmd == "id") {
+    char* id = nullptr;
+    app_get_id(&id);
+    if (id) {
+      result["data"] = picojson::value(id);
+      free(id);
+    }
+  } else if (cmd == "name") {
+    char* name = nullptr;
+    app_get_name(&name);
+    if (name) {
+      result["data"] = picojson::value(name);
+      free(name);
+    }
+  } else if (cmd == "version") {
+    char* version = nullptr;
+    app_get_name(&version);
+    if (version) {
+      result["data"] = picojson::value(version);
+      free(version);
+    }
+  } else if (cmd == "datapath") {
+    char* datapath = app_get_data_path();
+    if (datapath) {
+      result["data"] = picojson::value(datapath);
+      free(datapath);
+    }
+  } else if (cmd == "respath") {
+    char* respath = app_get_resource_path();
+    if (respath) {
+      result["data"] = picojson::value(respath);
+      free(respath);
+    }
+  } else if (cmd == "cachepath") {
+    char* cachepath = app_get_cache_path();
+    if (cachepath) {
+      result["data"] = picojson::value(cachepath);
+      free(cachepath);
+    }
+  } else if (cmd == "sharedrespath") {
+    char* sharedrespath = app_get_shared_resource_path();
+    if (sharedrespath) {
+      result["data"] = picojson::value(sharedrespath);
+      free(sharedrespath);
+    }
+  } else if (cmd == "appcontrol_response") {
+    HandleAppcontrolResponse(value, &result);
+  } else {
+    LOGW("Ignoring message. It is not an object.");
+    return;
+  }
+  SendSyncReply(picojson::value(result).serialize().c_str());
+}
+
+void AppCommonInstance::HandleAppcontrolResponse(const picojson::value& value,
+                                                 picojson::object* result) {
+  if (!value.get("appcontrol").is<std::string>() ||
+      !value.get("data").is<picojson::object>()) {
+    LOGE("Ignoring message. Wrong arguments were passed");
+    return;
+  }
+  std::string appcontrol_json = value.get("appcontrol").to_str();
+
+  // TODO(sngn.lee): to be implemented
+  // create request from json
+  // appcontrol_h request;
+  // app_control_create_from_json(&request, json.c_str());
+
+  auto extra_data = value.get("data").get<picojson::object>();
+  app_control_h response;
+  app_control_create(&response);
+  for (auto& item : extra_data) {
+    auto& key = item.first;
+    auto& value = item.second;
+    if (value.is<picojson::array>()) {
+      auto& array_value = value.get<picojson::array>();
+      std::vector<std::string> data_store;
+      std::vector<const char*> pointer_store;
+      for (auto& array_item : array_value) {
+        data_store.push_back(array_item.to_str());
+        pointer_store.push_back(data_store.back().c_str());
+      }
+      app_control_add_extra_data_array(response,
+          key.c_str(),
+          pointer_store.data(),
+          pointer_store.size());
+    } else {
+      app_control_add_extra_data(response,
+          key.c_str(),
+          value.to_str().c_str());
+    }
+  }
+
+  // TODO(sngn.lee): to be implemented
+  // app_control_reply_to_launch_request(response, request, 0);
+  // app_control_destroy(request);
+  app_control_destroy(response);
+  result->insert(std::make_pair("result", picojson::value("OK")));
+}
+
+}  // namespace appfw
+
+EXPORT_XWALK_EXTENSION(tizen_application_common, appfw::AppCommonExtension);
---
+++ modules/tizen-application/tizen-application_api.js
@@ -0,0 +1,211 @@
+'use strict';
+
+var async_message_id = 0;
+var async_map = new Map();
+
+function native_sync_call(method, parameter) {
+  var args = {};
+  args['cmd'] = method;
+  args = Object.assign(args, parameter);
+  try {
+    return JSON.parse(extension.internal.sendSyncMessage(JSON.stringify(args)));
+  } catch (e) {
+    console.log(e.message);
+    return {};
+  }
+}
+
+function native_async_call(method, parameter, cb) {
+  var args = {};
+  args['cmd'] = method;
+  args = Object.assign(args, parameter);
+  var asyncid = async_message_id++;
+  args['asyncid'] = 'asyncid_' + asyncid;
+  async_map.set(args['asyncid'], cb);
+  extension.postMessage(JSON.stringify(args));
+}
+
+function registEventHandler(app) {
+  var handler = (function(self) {
+    return function(json) {
+      var msg = JSON.parse(json);
+      if (msg['asyncid'] && async_map.has(msg['asyncid'])) {
+        var cb = async_map.get(msg['asyncid']);
+        async_map.delete(msg['asyncid']);
+        if (cb instanceof Function) {
+          cb(msg);
+        } else {
+          console.log('cb is not function');
+        }
+      } else {
+        self.__event_handle__(msg);
+      }
+    };
+  })(app);
+  extension.setMessageListener(handler);
+}
+
+var ApplicationCommon = require('tizen-application-common').ApplicationCommon;
+
+
+var started = Symbol();
+/**
+ * @class Application
+*  @extends ApplicationCommon
+ */
+class Application extends ApplicationCommon {
+  constructor() {
+    super();
+    this[started] = false;
+    registEventHandler(this);
+  }
+
+  __event_handle__(msg) {
+    if (msg['event'] == 'appcontrol') {
+      var RequestedAppControl =
+          require('tizen-application-common').RequestedAppControl;
+      var json = JSON.parse(msg['data']);
+      var request = new RequestedAppControl(json);
+      this.emit('appcontrol', request);
+    } else if (
+        [
+          'pause', 'resume', 'terminate', 'languagechange', 'lowmemory',
+          'lowbattery', 'regionchange', 'orientationchange'
+        ].indexOf(msg['event']) >= 0) {
+      this.emit(msg['event'], msg['data']);
+    } else {
+      console.log('invalid event was passed');
+    }
+  }
+
+   /**
+    * @method start
+    * @return {Promise<>}
+    */
+   start() {
+    if (this[started])
+      return Promise.reject(new Error('already started'));
+
+    this[started] = true;
+    return new Promise(function(resolve, reject) {
+      require('gcontext').init();
+      process.title = process.argv[1];
+      var args = {};
+      args['argv'] = process.argv.slice(1);
+      native_async_call('start', args, function(result) {
+        if (result['result'] == 'OK') {
+          resolve();
+        } else {
+          reject(new Error(result['reason']));
+        }
+      });
+    });
+   }
+
+  /**
+   * ....
+   * @event appcontrol
+   * @param {AppControl} appcontrol
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event pause
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event resume
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event terminate
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event lowmemory
+   * @param {string} status
+   * * "NORMAL"
+   * * "SOFT_WARNING"
+   * * "HARD_WARNING"
+   * @since 3.0
+   */
+
+
+  /**
+   * ....
+   * @event lowbattery
+   * @param {string} status
+   * * "POWER_OFF "
+   * * "CRITICAL_LOW"
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event languagechange
+   *
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event regionchange
+   * @param {string} language
+   *
+   * @since 3.0
+   */
+
+  /**
+   * ....
+   * @event orientationchange
+   * @param {string} orientation
+   *
+   * @since 3.0
+   */
+
+};
+
+
+/*
+ * @module tizen-application
+ *
+ * 'tizen-application' module exports a instance of Application class.
+ *
+ * ```
+ * var AppControl = require('tizeen-app-control');
+ *
+ * var app = require('tizen-application');
+ * app.on('appcontrol', function(requested){
+ *   if (requested.operation == AppControl.OPERATION_MAIN) {
+ *     // main ..
+ *   } else {
+ *     ....
+ *   }
+ * });
+ *
+ * app.on('pause', function() {
+ *   // pause ...
+ * });
+ *
+ * app.on('terminate', function(){
+ *   // release resources
+ * });
+ *
+ * app.start().then(function() {
+ *   console.log(app.name);
+ *   console.log(app.id);
+ *
+ * }).catch(function(e){
+ *   console.log(e.message);
+ * });
+ *
+ * ```
+ */
+exports = new Application();

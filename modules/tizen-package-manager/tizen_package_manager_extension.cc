19db9e9f7a7a94addf28cf864119e23e5043a1d5
JongHeon Choi <j-h.choi@samsung.com>
2016-01-12 19:21:31 +0900

    Add tizen-package-manager module
    Change-Id: Icbca46bacb836b119f2974ec811da821f48c4a14

---
+++ modules/tizen-package-manager/tizen_package_manager_extension.cc
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "tizen_package_manager_extension.h"
+
+#include <dlog.h>
+
+#include "picojson.h"
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#define LOG_TAG "JSNative"
+
+namespace packagemanager {
+
+xwalk::XWalkExtensionInstance* PackageManagerExtension::CreateInstance() {
+  return new PackageManagerInstance();
+}
+
+void PackageManagerInstance::Initialize() {
+  LOGD("Created tizen-package-manager instance");
+}
+
+void PackageManagerInstance::HandleMessage(const char* msg) { //async
+  // parse json object
+  picojson::value value;
+  std::string err;
+  picojson::parse(value, msg, msg + strlen(msg), &err);
+  if (!err.empty()) {
+    LOGE("Ignoring message. Can't parse msessage : %s", err.c_str());
+    return;
+  }
+  if (!value.is<picojson::object>()) {
+    LOGE("Ignoring message. It is not an object.");
+    return;
+  }
+
+  auto& request = value.get<picojson::object>();
+  auto found = request.find("asyncid");
+  if (found == request.end()) {
+    LOGE("asyncid was not existed");
+    return;
+  }
+
+  // handle synchronous messages
+  std::string cmd = value.get("cmd").to_str();
+  picojson::object result;
+
+  if (cmd == "") {
+
+  } else if (cmd == "") {
+
+  } else if (cmd == "") {
+
+  } else {
+    LOGW("Ignoring message. It is not an object.");
+    return;
+  }
+  SendSyncReply(picojson::value(result).serialize().c_str());
+}
+
+void PackageManagerInstance::HandleSyncMessage(const char* msg) {
+  // parse json object
+}
+
+void PackageManagerInstance::FireSimpleEvent(const std::string& event,
+                                             const std::string& data) {
+  picojson::value::object obj;
+  obj["event"] = picojson::value(event);
+  if (data.length() > 0) {
+    obj["data"] = picojson::value(data);
+  }
+  PostMessage(picojson::value(obj).serialize().c_str());
+}
+
+}  // namespace packagemanager
+
+EXPORT_XWALK_EXTENSION(tizen_package_manager, packagemanager::PackageManagerExtension);

8f77baf68d65a53c66a6308e4d5be6e098de6d99
rina6350.you <rina6350.you@samsung.com>
2016-01-20 19:20:40 +0900

    [SystemRuntimeInfo] add system system runtime info API.
    Change-Id: Id702c9e81924dde8e0db4046980e22502009449a

--- modules/tizen-system-runtime-info/tizen-system-runtime-info_api.js
+++ modules/tizen-system-runtime-info/tizen-system-runtime-info_api.js
@@ -5,9 +5,8 @@ function native_sync_call(method, parameter) {
   args['cmd'] = method;
   args = Object.assign(args, parameter);
   try {
-  	console.log("[sync call] 1");
-  	var ret = extension.internal.sendSyncMessage(JSON.stringify(args));
-	console.log("[sync call] 2");
+      var ret = extension.internal.sendSyncMessage(JSON.stringify(args));
+    console.log("[native_sync_call] after");
     return JSON.parse(ret);
   } catch (e) {
     console.log('recevied message parse error:' + e.message);
@@ -15,11 +14,53 @@ function native_sync_call(method, parameter) {
   }
 }
 
+function native_async_call(method, parameter, cb) {
+  var args = {};
+  args['cmd'] = method;
+  args = Object.assign(args, parameter);
+  var asyncid = async_message_id++;
+  args['asyncid'] = 'asyncid_' + asyncid;
+  async_map.set(args['asyncid'], cb);
+  extension.postMessage(JSON.stringify(args));
+}
+
+function registerEventHandler(app) {
+  var handler = (function(self) {
+    return function(json) {
+      var msg = JSON.parse(json);
+      if (msg['asyncid'] && async_map.has(msg['asyncid'])) {
+        var cb = async_map.get(msg['asyncid']);
+        async_map.delete(msg['asyncid']);
+        if (cb instanceof Function) {
+          cb(msg);
+        } else {
+          console.log('cb is not function');
+        }
+      } else {
+        self.__event_handle__(msg);
+      }
+    };
+  })(app);
+  extension.setMessageListener(handler);
+}
+
+
+var EventEmitter = require('events');
+
 /**
- * @class AppPreference
+ * @class SystemRuntimeInfo
  */
-class AppPreference {
+class SystemRuntimeInfo extends EventEmitter{
   constructor() {
+      console.log("[System-Runtime-Info] Constructor");
+	  super();
+	  registerEventHandler(this);
+  }
+  
+  __event_handle__(msg) {
+  	  console.log("__event_handle__ before :"+msg['event']+" "+msg['data']);
+      this.emit(msg['event'], msg['data']);
+	  console.log("__event_handle__ after");
   }
 
   /**
@@ -35,10 +76,21 @@ class AppPreference {
    * @param {string} key
    * @return {string}
    */
-  getItem(key) {
+  getValue(key) {
+    console.log("getValue 1");
     var result = native_sync_call('get', {'key': key});
-	
+    console.log("getValue 2");
     return result['data'];
+  }
+
+
+  /**
+   * @method getItem
+   * @param {string} key
+   * @return {string}
+   */
+  getItem(key) {
+    return native_sync_call('get', {'key': key})['data'];
   }
 
   /**
@@ -102,4 +154,4 @@ class AppPreference {
 
 };
 
-exports = new AppPreference();
+exports = new SystemRuntimeInfo();
